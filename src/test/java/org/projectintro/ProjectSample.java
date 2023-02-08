545a88a440121e8d1b2b22ff0a4a63b2fdd5f2e3
PraveenKumr2597 <pravnkumarsankar@gmail.com>
2023-02-08 11:14:09 +0530

    Dinesh's Code

--- src/test/java/org/projectintro/ProjectSample.java
+++ src/test/java/org/projectintro/ProjectSample.java
@@ -2,4 +2,11 @@ package org.projectintro;
 
 public class ProjectSample {
 
+	public static void main(String[] args) {
+		
+		System.out.println("Print the value of 5");
+		System.out.println("Print the value of 6");
+		System.out.println("Print the value of 7");
+		System.out.println("Print the value of 8");
+	}
 }
