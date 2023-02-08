c0e03f653d6ecf2d8d182bd186ed22786310f573
PraveenKumr2597 <pravnkumarsankar@gmail.com>
2023-02-08 11:46:09 +0530

    Sindhu's Code

--- src/test/java/org/projectintro/ProjectSample.java
+++ src/test/java/org/projectintro/ProjectSample.java
@@ -2,4 +2,10 @@ package org.projectintro;
 
 public class ProjectSample {
 
+	public static void main(String[] args) {
+		
+		System.out.println("Print the value 9");
+		System.out.println("Print the value 10");
+		System.out.println("Print the value 11");
+	}
 }
