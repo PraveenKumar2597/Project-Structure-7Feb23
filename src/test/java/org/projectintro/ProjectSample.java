801389aea34f42788457e3183dd507871d21aa2d
PraveenKumr2597 <pravnkumarsankar@gmail.com>
2023-02-08 11:30:47 +0530

    Priya's Code

--- src/test/java/org/projectintro/ProjectSample.java
+++ src/test/java/org/projectintro/ProjectSample.java
@@ -2,4 +2,10 @@ package org.projectintro;
 
 public class ProjectSample {
 
+	public static void main(String[] args) {
+		
+		System.out.println("Test 1");
+		System.out.println("Test 2");
+		System.out.println("Test 3");
+	}
 }
